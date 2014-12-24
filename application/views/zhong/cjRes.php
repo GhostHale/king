4e41b8be2b36afa9ad854566e3cf3a624c6174a6
魂狩 <a364717646@163.com>
2014-12-24 12:51:19 +0800

    修改文档，完善数据库
    Signed-off-by: 魂狩 <a364717646@163.com>

---
+++ application/views/zhong/cjRes.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>抽奖公告</title>
+<meta name="description" content="">
+<link rel="stylesheet" type="text/css" href="/css/lottery_detail.css">
+<?php include(APPPATH.'views/top.php');?>
+	<div id="main">
+		<span class="main_nav"><a href="">首页</a> > <a href="">我要投资</a> > <a href="">产品介绍</a></span>
+		<div class="content">
+			<h1>【某某项目】抽奖公告</h1>
+			<h2>时间：2014年1月1日</h2>
+			<span class="red t">7大城市11套1.1折房源幸运折全部产生，恭喜中奖的用户！</span>
+			<ul>
+				<li>活动页面：<span></span></li>
+				<li>活动时间：<span></span></li>
+				<li>抽奖规则：<span></span></li>
+				<li>抽奖号查询：<span></span></li>
+				<li>双色球开奖日期：<span></span></li>
+				<li>幸运用户：<span></span></li>
+			</ul>
+			<table>
+				<tr>
+					<th>房产名称</th>
+					<th>参与抽奖人数</th>
+					<th>中奖号</th>
+					<th>中奖用户</th>
+				</tr>
+				<tr>
+					<td>北京</td>
+					<td>111</td>
+					<td>111</td>
+					<td>111</td>
+				</tr>
+				<tr>
+					<td>北京</td>
+					<td>111</td>
+					<td>111</td>
+					<td>111</td>
+				</tr>
+				<tr>
+					<td>北京</td>
+					<td>111</td>
+					<td>111</td>
+					<td>111</td>
+				</tr>
+				<tr>
+					<td>北京</td>
+					<td>111</td>
+					<td>111</td>
+					<td>111</td>
+				</tr>
+				<tr>
+					<td>北京</td>
+					<td>111</td>
+					<td>111</td>
+					<td>111</td>
+				</tr>
+			</table>
+			<span class="red">抽奖活动全程再xx监督下进行抽取。</span>
+			<span class="red">中奖名单公布后三个工作日，我们会将您的订单基本信息提供给远洋地产，届时将有客服联系你。</span>
+		</div>
+	</div>
+<?php include(APPPATH.'views/foot.php');?>
